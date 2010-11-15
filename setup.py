7a354376cc2cfbf30fb76c0c8ae4993955ff824c
Ruslan Popov <ruslan.popov@gmail.com>
2010-11-15 16:46:23 +0300

    Up.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='nezabudka',
-      version='0.1',
+      version='0.2',
       description='A simple Django application to integrate Ticket Tracking System into your projects',
       author='Ruslan Popov',
       author_email='ruslan.popov@gmail.com',
