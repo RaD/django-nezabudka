35683151a5275f0931f50bf3ac7908b63373e4da
Ruslan Popov <ruslan.popov@gmail.com>
2011-03-03 00:53:13 +0300

    More info about a ticket.

--- nezabudka/handlers.py
+++ nezabudka/handlers.py
@@ -18,9 +18,24 @@ class CommentResource(AnonymousBaseHandler):
     allowed_methods = ('GET',)
 
     def read(self, request, ticket_id):
+        out = {}
         ticket = models.Ticket.objects.get(id=int(ticket_id))
+        out['ticket'] = {'id': ticket.id,
+                         'title': ticket.title,
+                         'assigned_to_id': ticket.assigned_to.id,
+                         'assigned_to_username': ticket.assigned_to.username,
+                         'status_id': ticket.status.id,
+                         'status_title': ticket.status.title,
+                         'component_id': ticket.component.id,
+                         'component_title': ticket.component.title,
+                         'priority_id': ticket.priority.id,
+                         'priority_title': ticket.priority.title,
+                         'severity_id': ticket.severity.id,
+                         'severity_title': ticket.severity.title,
+                         }
         comments = models.Comment.objects.filter(ticket=ticket)
-        out = [{'id': o.id, 'text': o.text} for o in comments]
+        out['comments'] = [{'id': o.id, 'text': o.text, 'user': o.user.username,
+                            'date': o.reg_datetime} for o in comments]
         return out
 
 class MediaResource(AnonymousBaseHandler):
--- static/js/engine.js
+++ static/js/engine.js
@@ -1,21 +1,36 @@
 var engine = {
+
+    window_width: 0,
+
     init: function() {
+        _this = this;
+        $(window).bind('load resize', function() {
+            _this.window_width = w = $(this).width() - 50;
+            tds = $('#content table td');
+            $(tds[0]).css({width: _this.pct_of_width(w, 0.1)});
+            $(tds[1]).css({width: _this.pct_of_width(w, 0.4)});
+            $(tds[2]).css({width: _this.pct_of_width(w, 0.5)});
+        });
         this.tickets();
         this.statuses();
+    },
+    pct_of_width: function(width, percent) {
+        return Math.floor(width * percent) + 'px'
     },
     tickets: function() {
         _this = this;
         $.get('/nezabudka/tickets/', {},
               function(json) {
-                  var content = $('#ticket_list');
+                  var content = $('#ticket_list'),
+                      _width = content.width();
                   $.each(json, function() {
                       var item = this;
                       var click_handler = function() { _this.show_ticket(item.id); }
-                      var dom_object = $('<div/>')
-                                       .addClass('listitem')
-                                       .text('Ticket '+item.id+': '+  item.title)
-                                       .click(click_handler);
-                      content.append(dom_object);
+                      var div = $('<div/>').addClass('listitem').click(click_handler)
+                      .text('Ticket '+item.id+': '+  item.title)
+                      .attr({alt: item.title, title: item.title})
+                      .css({width: (_width - 4) + 'px'});
+                      content.append(div);
                   });
               });
     },
@@ -27,7 +42,7 @@ var engine = {
                       var id = json[i].id,
                           title = json[i].title,
                           item = $('<div/>')
-                                   .addClass('listitem')
+                                   .addClass('selector_item')
                                    .text(title)
                                    .click(function() { alert(id) });
                       content.append(item);
@@ -35,19 +50,25 @@ var engine = {
               });
     },
     show_ticket: function(ticket_id) {
+        _this = this;
         var content = $('#item_info');
-        $('div', content).each(function() { $(this).remove(); })
+        var comments = $('#comment_list');
         $.get('/nezabudka/comments/' + ticket_id + '/', {},
               function(json) {
-                  for (var i=0; i<json.length; i++) {
-                      var id = json[i].id,
-                          desc = json[i].text,
-                          item = $('<div/>')
-                                   .addClass('comment_item')
-                                   .text(desc)
-                                   .click(function() { alert(id) });
-                      content.append(item);
-                  }
+                  $('#title', content).text(json.ticket.title);
+                  $('#component > span', content).text(json.ticket.component_title);
+
+                  $('div', comments).each(function() { $(this).remove(); })
+                  $.each(json.comments, function() {
+                      header = $('<div/>').html('<b>' + this.user + '</b> @ '
+                                                + '<b>' + this.date + '</b><br/>'
+                                                + this.text);
+                      comments.append($('<div/>')
+                                      .append(header)
+                                      .addClass('comment_item')
+                                      .css({width: _this.pct_of_width(_this.window_width, 0.45)})
+                                     );
+                  });
               });
     }
 }
